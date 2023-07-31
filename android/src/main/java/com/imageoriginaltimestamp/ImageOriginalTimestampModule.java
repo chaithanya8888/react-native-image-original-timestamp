01f7fad950d134d529d1db4ebeb4fbcdfa1fea2e
chaithanya8888 <95884449+chaithanya8888@users.noreply.github.com>
2023-07-31 21:06:15 +0530

    Android Fix

--- android/src/main/java/com/imageoriginaltimestamp/ImageOriginalTimestampModule.java
+++ android/src/main/java/com/imageoriginaltimestamp/ImageOriginalTimestampModule.java
@@ -2,11 +2,17 @@ package com.imageoriginaltimestamp;
 
 import androidx.annotation.NonNull;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+
+import android.media.ExifInterface;
 
 @ReactModule(name = ImageOriginalTimestampModule.NAME)
 public class ImageOriginalTimestampModule extends ReactContextBaseJavaModule {
@@ -22,11 +28,22 @@ public class ImageOriginalTimestampModule extends ReactContextBaseJavaModule {
     return NAME;
   }
 
-
   // Example method
   // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+  public void fetchTimeStamp(String url, Promise promise) {
+    try {
+      ExifInterface exif = new ExifInterface(url);
+      String str_date = exif.getAttribute(ExifInterface.TAG_DATETIME_ORIGINAL);
+      if (str_date != null) {
+        DateFormat formatter = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+        Date date = (Date) formatter.parse(str_date);
+        promise.resolve(String.valueOf(date.getTime() / 1000));
+      } else {
+        promise.reject(new Error("No Exif Data Available"));
+      }
+    } catch (Exception e) {
+      promise.reject(new Error(e.toString()));
+    }
   }
 }
