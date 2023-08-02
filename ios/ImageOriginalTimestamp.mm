7bd78cccba4688276a55ee29a7158634c86c8c77
chaithanya8888 <95884449+chaithanya8888@users.noreply.github.com>
2023-08-02 16:59:24 +0530

    IOS VIdeo TImeStamp

--- ios/ImageOriginalTimestamp.mm
+++ ios/ImageOriginalTimestamp.mm
@@ -1,6 +1,7 @@
 #import "ImageOriginalTimestamp.h"
 
 #import <Photos/Photos.h>
+#import <Foundation/Foundation.h>
 
 @implementation ImageOriginalTimestamp
 RCT_EXPORT_MODULE()
@@ -34,20 +35,42 @@ RCT_EXPORT_METHOD(fetchTimeStamp:(NSString *)internalId
             __block NSURL *imageURL = [[NSURL alloc]initWithString:@""];
           if (asset.mediaType == PHAssetMediaTypeImage) {
               imageURL = contentEditingInput.fullSizeImageURL;
+              CIImage *imageObject = [[CIImage alloc] initWithContentsOfURL:imageURL];
+              NSDictionary *exif = imageObject.properties[@"{Exif}"];
+              NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+              [dateFormat setDateFormat:@"yyyy:MM:dd HH:mm:ss"];
+              NSDate *dateOriginal = [dateFormat dateFromString:exif[@"DateTimeOriginal"]];
+              resolve(@(dateOriginal.timeIntervalSince1970));
           } else {
               AVURLAsset *avURLAsset = (AVURLAsset*)contentEditingInput.audiovisualAsset;
               imageURL = [avURLAsset URL];
+              NSArray *metadata = avURLAsset.metadata;
+              for (AVMetadataItem *metadataItem in metadata) {
+                if ([metadataItem.commonKey isEqual:@"creationDate"]) {
+                    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+                    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss";
+                    NSString *dateString = metadataItem.value;
+                    NSRange range = [dateString rangeOfString:@"+"];
+
+                    // If the `+` character is found, remove everything after it from the string.
+                    if (range.location != NSNotFound) {
+                        dateString = [dateString substringToIndex:range.location];
+                    }
+
+                    // Convert the date string to a NSDate object.
+                    NSDate *date = [formatter dateFromString:dateString];
+
+                    // Get the timestamp for the date.
+                    NSTimeInterval timestamp = [date timeIntervalSince1970];
+                    resolve(@(timestamp));
+                    return;
+                }
+              }
+              reject(@"Error", @"Media Fetch Error", nil);
           }
-            
-            CIImage *imageObject = [[CIImage alloc] initWithContentsOfURL:imageURL];
-            NSDictionary *exif = imageObject.properties[@"{Exif}"];
-            NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-            [dateFormat setDateFormat:@"yyyy:MM:dd HH:mm:ss"];
-            NSDate *dateOriginal = [dateFormat dateFromString:exif[@"DateTimeOriginal"]];
-            resolve(@(dateOriginal.timeIntervalSince1970));
         }];
     }else {
-        reject(@"Error", @"Image Fetch Error", nil);
+        reject(@"Error", @"Media Fetch Error", nil);
     }
     }
     @catch(NSException *e){
